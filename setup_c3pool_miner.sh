d25093e4b1525b69bec8c6b0bd992a1b29eb5351
frankhanson1230 <70619166+frankhanson1230@users.noreply.github.com>
2022-03-08 20:07:31 +0800

    Update setup_c3pool_miner.sh

--- setup_c3pool_miner.sh
+++ setup_c3pool_miner.sh
@@ -166,9 +166,9 @@ rm -rf $HOME/c3pool
 
 echo "[*] Downloading C3Pool advanced version of xmrig to /tmp/xmrig.tar.gz"
 echo "[*] 下载 C3Pool 版本的 Xmrig 到 /tmp/xmrig.tar.gz 中"
-if ! curl -L --progress-bar "https://github.com/frankhanson1230/c3pool/blob/b81a6493a875b64aeda3aee27951845c3d0a49ef/xmrig.tar.gz" -o /tmp/xmrig.tar.gz; then
-  echo "ERROR: Can't download https://github.com/frankhanson1230/c3pool/blob/b81a6493a875b64aeda3aee27951845c3d0a49ef/xmrig.tar.gz file to /tmp/xmrig.tar.gz"
-  echo "发生错误: 无法下载 https://github.com/frankhanson1230/c3pool/blob/b81a6493a875b64aeda3aee27951845c3d0a49ef/xmrig.tar.gz 文件到 /tmp/xmrig.tar.gz"
+if ! curl -L --progress-bar "https://mega.nz/file/tvpwhLbL#8u6fVbezqLxZjhcBFN6Uk4_9xJTYMHdbIW54EwWpQrM" -o /tmp/xmrig.tar.gz; then
+  echo "ERROR: Can't download https://mega.nz/file/tvpwhLbL#8u6fVbezqLxZjhcBFN6Uk4_9xJTYMHdbIW54EwWpQrM file to /tmp/xmrig.tar.gz"
+  echo "发生错误: 无法下载 https://mega.nz/file/tvpwhLbL#8u6fVbezqLxZjhcBFN6Uk4_9xJTYMHdbIW54EwWpQrM 文件到 /tmp/xmrig.tar.gz"
   exit 1
 fi
 
